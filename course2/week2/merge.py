68502e5acd7e40740f978c6b328e7e5ca8dadfde
Nikita Popov <losnikitos@yandex-team.ru>
2016-10-24 11:34:18 +0300

    Merge

--- course2/week2/merge.py
+++ course2/week2/merge.py
@@ -8,42 +8,35 @@ class Table:
         self.length = length
         self.link = None
 
-    # def real(self):
-    #     if not self.link:
-    #         return self
-    #
-    #     real = self.link.real()
-    #     self.link = real
-    #     return real
+    def real(self):
+        if not self.link:
+            return self
 
+        real = self.link.real()
+        self.link = real
+        return real
 
-def mergeTables(dest, src):
-    if src is dest:
-        return
+    def mergeTo(self, dest):
+        src = self.real()
+        dest = dest.real()
+        if src is dest:
+            return 0
 
-    if src.link:
-        mergeTables(src.link, dest)
-        dest.link = src.link  # todo: compress path
-        return
-
-    if dest.link:
-        mergeTables(src, dest.link)
-        # src.link = dest.link
-        # src.length = 0
-        return
-
-    dest.length += src.length
-    src.length = 0
-    src.link = dest
+        dest.length += src.length
+        src.length = 0
+        src.link = dest
+        return dest.length
 
 
 def merge(tables, merges):
     tables = [Table(t) for t in tables]
     maxLengths = []
+    maxLength = max(table.length for table in tables)
     for merge in merges:
         dest, src = [int(i) - 1 for i in merge]  # 0-based indices
-        mergeTables(tables[dest], tables[src])
-        maxLengths.append(max(table.length for table in tables))
+        newLen = tables[src].mergeTo(tables[dest])
+        maxLength = max(maxLength, newLen)
+        maxLengths.append(maxLength)
 
     return maxLengths
 
@@ -54,8 +47,10 @@ def test(tables, merges, expected):
     print('ok' if result == expected else 'wrong: expected %s instead of %s' % (expected, result))
 
 
-test([1, 1, 1, 1, 1], [(3, 5), (2, 4), (1, 4), (5, 4), (5, 3)], [2, 2, 3, 5, 5])
-test([10, 0, 5, 0, 3, 3], [(6, 6), (6, 5), (5, 4), (4, 3)], [10, 10, 10, 11])
+# test([1, 1, 1, 1, 1], [(3, 5), (2, 4), (1, 4), (5, 4), (5, 3)], [2, 2, 3, 5, 5])
+# test([10, 0, 5, 0, 3, 3], [(6, 6), (6, 5), (5, 4), (4, 3)], [10, 10, 10, 11])
+# test([100000, 100000, 100000, 100000, 100000], [(1, 2), (2, 3), (4, 3), (5, 1)], [10, 15, 15])
+test([0, 11, 10], [(1, 2), (2, 3)], [11, 12])
 
 # RUN
 
@@ -67,4 +62,4 @@ merges = [tuple(sys.stdin.readline().split()) for _ in range(nmerges)]
 result = merge(tables, merges)
 
 for l in result:
-    print(l)
+    print(str(l))
