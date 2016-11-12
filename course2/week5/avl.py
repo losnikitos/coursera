17d01b6f4e211b0b8e64aed4fee1981d292c3316
Nikita Popov <losnikitos@yandex-team.ru>
2016-11-13 02:05:49 +0300

    Avl

--- course2/week5/avl.py
+++ course2/week5/avl.py
@@ -135,6 +135,7 @@ root = None
 
 def insert(x):
     global root
+    # print('# insert', x)
     (left, right) = split(root, x)
     new_vertex = None
     if right == None or right.key != x:
@@ -142,76 +143,36 @@ def insert(x):
     root = merge(merge(left, new_vertex), right)
 
 
-def erase(key):
+def erase(x):
     global root
-
-    # print('erase', key)
-
-    node, _ = find(root, key)
-    if node is None or node.key != key:
-        return
-
-    next_node = next(node)
-
-    if next_node is None:
-        # print('no next node')
-        splay(node)
-        root = node.left
-        if root:
-            root.parent = None
-        return
-
-    root = splay(next_node)
-    root = splay(node)
-    # node is root, next_node is on his right
-    next_node.left = node.left
-    if node.left:
-        node.left.parent = next_node
-
-    root = next_node
-    # TODO: update?
+    # print('# erase', x)
+    (left, middle) = split(root, x)
+    (middle, right) = split(middle, x + 1)
+    root = merge(left, right)
 
 
 def search(x):
-    global root, last_sum_result
-    (found, _) = find(root, x)
-
-    if not found or found.key != x:
-        return None
+    global root
+    (node, root) = find(root, x)
+    if node and node.key == x:
+        return node
     else:
-        return found
+        return False
 
 
 def sum(fr, to):
     global root
+    # print('# sum from', fr, 'to', to)
     (left, middle) = split(root, fr)
     (middle, right) = split(middle, to + 1)
 
-    update(middle)
-    return middle.sum
-
-def next(n):
-    if not n:
-        return None
-
-    if not n.right:
-        current = n.parent
-        if current is None:
-            return None
-        while current.key < n.key and current is not None:
-            current = current.parent
-        return current
-    else:
-        current = n.right
-        while current.left:
-            current = current.left
-        return current
+    ans = middle.sum if middle else 0
+    root = merge(merge(left, middle), right)
+    return ans
 
 
 def apply_operation(line):
-    global last_sum_result
-
-    # print('operation', line)
+    global last_sum_result, root
     line = line.split()
     if line[0] == '+':
         x = int(line[1])
@@ -235,7 +196,69 @@ def apply_operations(lines):
     return [str(res) for res in results if res is not None]
 
 
-# TEST
+def listNodes(node):
+    return listNodes(node.left) + [node.key] + listNodes(node.right) if node else []
+
+
+def printTreeList(node):
+    print()
+    nodes = listNodes(node)
+    nodes.sort()
+    for n in nodes:
+        print(n)
+    print()
+    return
+
+
+def printTree(node, depth=0):
+    output = ""
+    prefix = ("   " * depth if depth else '*')
+
+    if not node:
+        if depth == 0:
+            print('*')
+            return
+        else:
+            return prefix + '-\n'
+
+    if depth == 0:
+        print('')
+
+    output += printTree(node.right, depth + 1)
+    output += prefix + str(node.key) + '(' + str(node.sum) + ')^' + (
+        str(node.parent.key) if node.parent else '*') + '\n'
+    output += printTree(node.left, depth + 1)
+
+    if depth == 0:
+        print(output[:-1])
+        print('')
+
+    return output
+
+
+def interactive():
+    global root
+    while True:
+        apply_operation(stdin.readline())
+        printTree(root)
+
+
+def test_step(operations, expected):
+    global last_sum_result, root
+    last_sum_result = 0
+    root = None
+    for operation in operations:
+        res = str(apply_operation(operation))
+        if res == 'None':
+            continue
+        right_answer = expected[0]
+        expected = expected[1:]  # unshift
+        if res == right_answer:
+            continue
+        else:
+            raise RuntimeError('wrong: got %s, expected %s' % (res, right_answer))
+    print('ok')
+
 
 def test(operations, expected):
     global last_sum_result, root
@@ -243,6 +266,14 @@ def test(operations, expected):
     root = None
     result = list(apply_operations(operations))
     print('ok' if result == expected else 'wrong: expected %s instead of %s' % (expected, result))
+
+
+def run():
+    n = int(stdin.readline())
+    lines = [stdin.readline() for _ in range(n)]
+    result = apply_operations(lines)
+    for line in result:
+        print(line)
 
 
 set1 = ['? 1',
@@ -276,14 +307,177 @@ res2 = ['Not found', 'Found', 'Not found']
 set3 = ['+ 491572259', '? 491572259', '? 899375874', 's 310971296 877523306', '+ 352411209']
 res3 = ['Found', 'Not found', '491572259']
 
+set4 = ['+ 5', '+ 4', '+ 3', '+ 2', '+ 1', '+ 18', '+ 15', 's 3 5', 's 3 5']
+res4 = ['12', '0']
+
+set5 = ['s 40279559 89162572',
+        '- 774613289',
+        's 869592654 915517087',
+        '- 165280355',
+        '- 776346290',
+        '- 221187096',
+        's 421986248 742826969',
+        's 83228103 852190011',
+        '- 640319482',
+        '? 528689193',
+        '? 75245219',
+        '- 617070033',
+        '+ 66257759',
+        's 25751289 70170547',
+        's 28248247 617849094',
+        '- 954357244',
+        '+ 477444954',
+        '? 608389416',
+        's 400483980 423330836',
+        '- 477444954',
+        '? 441393551',
+        's 66257759 66257759',
+        '- 822218158',
+        '? 806479414',
+        's 548665149 925635534',
+        's 66257759 66257759',
+        '? 234121006',
+        '+ 663305907',
+        's 314809050 685231317',
+        '- 0',
+        's 487458874 602635501',
+        's 66257759 66257759',
+        '? 918193520',
+        '? 606474691',
+        's 188185089 774086933',
+        '- 322445571',
+        's 66257759 66257759',
+        '- 814123984',
+        's 0 0',
+        's 0 0',
+        's 689260392 827869844',
+        '? 204276815',
+        '- 66257759',
+        '? 488766408',
+        's 412617563 631410280',
+        '- 463415495',
+        '+ 601030115',
+        '? 776513589',
+        's 257003372 887483600',
+        '+ 154047223',
+        '? 154047223',
+        '? 219327735',
+        '+ 978812473',
+        's 978812473 154047223',
+        '? 718062555',
+        '? 128066784',
+        '- 15718305',
+        '? 754978417',
+        's 643892549 819127300',
+        '? 192401474',
+        '? 643892549',
+        '+ 638898307',
+        '? 973173529',
+        '+ 506709268',
+        '- 506709268',
+        '+ 744166533',
+        '- 638898307',
+        '+ 95240753',
+        's 997348833 63778002',
+        '? 31190791',
+        's 21011834 570648768',
+        '+ 217208615',
+        '+ 401912531',
+        's 0 723886547',
+        '? 251082460',
+        '+ 542593404',
+        's 702430665 542593404',
+        '? 48285749',
+        's 831077135 671239874',
+        '+ 917941607',
+        '? 908494561',
+        '? 671239874',
+        's 333354822 490605331',
+        '+ 261522346',
+        's 170201520 10364259',
+        '- 139162050',
+        '- 677374727',
+        '? 992422786',
+        '? 500171144',
+        '- 239436034',
+        '+ 556867643',
+        '? 992422786',
+        '+ 720003678',
+        's 220110584 268880636',
+        's 31190791 997548180',
+        's 898610232 383552107',
+        '- 682670734',
+        '+ 547596765',
+        's 496810115 875859347',
+        '? 41728941']
+res5 = ['0',
+        '0',
+        '0',
+        '0',
+        'Not found',
+        'Not found',
+        '66257759',
+        '0',
+        'Not found',
+        '0',
+        'Not found',
+        '66257759',
+        'Not found',
+        '0',
+        '66257759',
+        'Not found',
+        '729563666',
+        '0',
+        '66257759',
+        'Not found',
+        'Not found',
+        '0',
+        '66257759',
+        '66257759',
+        '66257759',
+        '0',
+        'Not found',
+        'Not found',
+        '0',
+        'Not found',
+        '601030115',
+        'Found',
+        'Not found',
+        '1935950040',
+        'Not found',
+        'Not found',
+        'Not found',
+        '1935950040',
+        'Not found',
+        'Found',
+        'Not found',
+        '0',
+        'Found',
+        '31190791',
+        '3328760130',
+        'Found',
+        '4200113661',
+        'Found',
+        '4200113661',
+        'Not found',
+        'Found',
+        '1860989273',
+        '4440680541',
+        'Found',
+        'Not found',
+        'Found',
+        '0',
+        '4220898514',
+        '1565728674',
+        '829624590',
+        'Found']
+
 # test(set1, res1)
 # test(set2, res2)
 # test(set3, res3)
+# test(set4, res4)
+# test_step(set5, res5)
 
-# RUN
+# interactive()
 
-n = int(stdin.readline())
-lines = [stdin.readline() for _ in range(n)]
-result = apply_operations(lines)
-for line in result:
-    print(line)
+run()
